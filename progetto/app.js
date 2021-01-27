be76e6f0c064d72ec8e80ba801f9795a18f20d5c
alessandro-corradino <corradino.alessandro@iisgalvanimi.edu.it>
2021-01-27 13:20:54 +0000

    Parte 3

---
+++ progetto/app.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+var sust1Router = require('./routes/sust1');
+var sust2Router = require('./routes/sust2');
+var app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+app.use('/sust1', sust1Router);
+app.use('/sust2', sust2Router);
+module.exports = app;
---
+++ progetto/routes/sust1.js
@@ -0,0 +1,24 @@
+var express = require('express');
+var router = express.Router();
+
+const MongoClient = require('mongodb').MongoClient; //Importo la libreria mongodb
+
+
+
+router.get('/', function (req, res, next) {
+    console.log(req.params); //Leggo i parametri passati all'url
+    title = req.params.title;
+    const uri = 'mongodb+srv://MolinariDaniele:danielE118@cluster0.dn3hp.mongodb.net/Cluster0?retryWrites=true&w=majority'
+    const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+    client.connect(err => {
+        const collection = client.db("WORLD_DATA_BANK").collection("sustainable_cities1"); //Mi connetto alla collection movies
+        // perform actions on the collection object
+        collection.find().limit(10).toArray((err, result) => {
+            if (err) console.log(err.message); //Se c'è qualche errore lo stampo
+            else res.send(result);
+            client.close(); //Quando ho terminato la find chiudo la sessione con il db
+        }); //Eseguo la query e passo una funzione di callback
+
+    });
+});
+module.exports = router;
